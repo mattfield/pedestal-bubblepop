8c4a6d76429d4ce5498769de2d9597eff2df1244
Matt Field <m.field42@gmail.com>
2013-10-12 14:41:11 +0100

    adds a button to data ui that increments counter 	modified:   app/src/bubblepop_client/behavior.clj

--- bubblepop-client/app/src/bubblepop_client/behavior.clj
+++ bubblepop-client/app/src/bubblepop_client/behavior.clj
@@ -1,11 +1,17 @@
 (ns ^:shared bubblepop-client.behavior
     (:require [clojure.string :as string]
-              [io.pedestal.app.messages :as msg]))
+              [io.pedestal.app.messages :as msg]
+              [io.pedestal.app :as app]))
 
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 (def example-app
   {:version 2
-   :transform [[:inc [:my-counter] inc-transform]]})
+   :transform [[:inc [:my-counter] inc-transform]]
+   :emit [{:init init-main}
+          [#{[:*]} (app/default-emitter [:main])]]})
 
